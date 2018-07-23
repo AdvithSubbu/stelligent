4b5f0a178628aa43cf454400f7bfed6dcac8c90d
kiran <kiran.kota@gmail.com>
2018-07-22 21:29:57 -0600

    Automated Test Script

--- scripts/teststack.py
+++ scripts/teststack.py
@@ -1,4 +1,22 @@
 import boto3
+import sys
+import requests
+
 client = boto3.client('cloudformation',)
 stack= client.describe_stacks(StackName='test111222')
 print( "DNS : " + stack['Stacks'][0]['Outputs'][0].get('OutputValue'))
+
+loadbalancerDnsName = stack['Stacks'][0]['Outputs'][0].get('OutputValue')
+if not loadbalancerDnsName :
+    print("Error getting LoadBalancer Dns")
+    sys.exit()
+    
+r = requests.get(loadbalancerDnsName)
+if r.status_code == 200:
+    print("WEbsite reachable")
+else:
+    print("website not reachable")
+    
+
+
+
